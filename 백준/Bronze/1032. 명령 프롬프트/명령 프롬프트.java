3d420578b6c405e792bd10b0ee02475294767f62
happysloth0908 <sozzykim@naver.com>
2025-01-08 23:41:27 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
@@ -2,38 +2,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		String arr [] = new String [N];
-		for (int i = 0; i < N; i++) {
-			arr[i] = br.readLine();
-		}
-//		System.out.println("문자열 길이는 " + arr[2].length());
-//		System.out.println(Arrays.toString(arr));
-//		StringBuilder sb = new StringBuilder();
-		String ans = arr[0];
-		for (int i = 0; i < arr[0].length(); i++) {
-//			System.out.println("1: " +  i + "번째 글씨 비교 시작");
-			for (int j = 1; j < N; j++) {
-//				System.out.println("2: " + j + "번째 문자열 검사");
-				if( ans.charAt(i) != arr[j].charAt(i)) {
-//					System.out.println("3: " + i + "번째 글씨가 다르다!!");
-//					System.out.println("4: " + ans.charAt(i) + "를 ?로 바꾼다!");
-//					ans = ans.replace(ans.charAt(i), '?'); // 이렇게 하면 다 교체해버림
-					ans = ans.substring(0, i) + "?" + ans.substring(i+ 1);
-//					System.out.println("5: " + "그럼 현재 ans 는 " + ans);
-					break;
+		char[] pattern = br.readLine().toCharArray();
+		
+		for (int i = 1; i < N; i++) {
+			char[] curr = br.readLine().toCharArray();
+			for (int j = 0; j < curr.length; j++) {
+				if(pattern[j] != curr[j]) {
+					pattern[j] = '?';
 				}
 			}
-//			sb.append(arr[j].charAt(i));
-			
 		}
+		String ans = new String(pattern);
 		System.out.println(ans);
 	}
 }
