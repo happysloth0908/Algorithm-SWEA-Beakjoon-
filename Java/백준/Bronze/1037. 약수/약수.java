be274ff92d195cc7089dd4c54e45b91740964bd2
happysloth0908 <sozzykim@naver.com>
2025-01-09 15:10:29 +0900

    [Bronze I] Title: 약수, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

--- Java/백준/Bronze/1037. 약수/약수.java
+++ Java/백준/Bronze/1037. 약수/약수.java
@@ -1,25 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collection;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // 제일 큰 수와 제일 작은 수 곱하면 됨.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        PriorityQueue<Integer> pqr = new PriorityQueue<>((a,b) -> b-a);
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        
         for (int i = 0; i < N; i++) {
-            int a = Integer.parseInt(st.nextToken());
-            pq.add(a);
-            pqr.add(a);
+            int num = Integer.parseInt(st.nextToken());
+            min = Math.min(min, num);
+            max = Math.max(max, num);
         }
-        int a = pq.poll();
-        int b = pqr.poll();
-        System.out.println(a * b);
+        
+        System.out.println(min * max);
     }
 }
