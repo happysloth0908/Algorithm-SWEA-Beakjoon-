c9e99b1fd96b2789fec784a781841fe5f9cfbb68
happysloth0908 <sozzykim@naver.com>
2025-09-30 12:31:27 +0900

    [Gold IV] Title: 연구소, Time: 1480 ms, Memory: 35004 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,61 +1,75 @@
-import sys
+from sys import stdin
 from collections import deque
 from itertools import combinations
 import copy
 
-input = sys.stdin.readline
-n, m = map(int, input().split())
+N,M = map(int, stdin.readline().split())
 
-graph = []
-blank = []
-virus = []
+lab = []
+
+for _ in range(N):
+    lab.append(list(map(int, stdin.readline().split())))
+
 dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+dy = [0, 0, 1, -1]
 
-def bfs(v, copy_graph):
-    visited = [[False]*m for _ in range(n)]
-    for k in v:
-        q = deque()
-        q.append((k[0], k[1]))
-        visited[k[0]][k[1]] = True
-        while q:
-            x, y = q.popleft()
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if 0 <= nx < n and 0 <= ny < m:
-                    if copy_graph[nx][ny] == 1:
-                        visited[nx][ny] = True
-                        continue
-                    elif graph[nx][ny] == 0:
-                        if visited[nx][ny] == False:
-                            copy_graph[nx][ny] = 2
-                            visited[nx][ny] = True
-                            q.append((nx, ny))
-    count = 0
-    for i in range(n):
-        for j in range(m):
-            if copy_graph[i][j] == 0:
-                count += 1
-    return count
+def bfs():
+    que = deque()
 
-for i in range(n):
-    data = list(map(int, input().split()))
-    for j in range(m):
-        if data[j] == 0:
-            blank.append([i, j])
-        elif data[j] == 2:
-            virus.append([i, j])
-    graph.append(data)
+    for i in range(N):
+        for j in range(M):
+            if lab[i][j] == 2:
+                que.append((i,j))
 
-max_count = 0
-for combi in list(combinations(blank, 3)):
-    copy_graph = copy.deepcopy(graph)
-    for c in combi:
-        x, y = c
-        copy_graph[x][y] = 1
-    count = bfs(virus, copy_graph)
-    max_count = max(max_count, count)
+    infected_lab = [row[:] for row in lab]
+
+    while que:
+        x,y = que.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx >= N or ny >= M or nx < 0 or ny < 0 : continue
+            if infected_lab[nx][ny] == 0:
+                infected_lab[nx][ny] = 2
+                que.append((nx,ny))
+
+    cnt = sum(row.count(0) for row in infected_lab)
+    global answer
+    answer = max(answer, cnt)
+
+# def wall(cnt):
+#
+#     if cnt >= 3:
+#         bfs()
+#         return
+#
+#     for i in range(N):
+#         for j in range(M):
+#             if lab[i][j] == 0:
+#                 lab[i][j] = 1
+#                 wall(cnt + 1)
+#                 lab[i][j] = 0
+answer = 0
+
+blanks = []
+
+for i in range(N):
+    for j in range(M):
+      if lab[i][j] == 0:
+          blanks.append((i,j))
+
+for (x,y), (x2, y2), (x3, y3) in combinations(blanks, 3):
+    lab[x][y] = 1
+    lab[x2][y2] = 1
+    lab[x3][y3] = 1
+    bfs()
+    lab[x][y] = 0
+    lab[x2][y2] = 0
+    lab[x3][y3] = 0
 
 
-print(max_count)
+
+
+print(answer)
