4ac5f8a51e6926b202672c0c5a762f0eee2d81d4
happysloth0908 <sozzykim@naver.com>
2026-01-31 17:12:23 +0900

    [level 2] Title: 더 맵게, Time: 1744.66 ms, Memory: 48.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,18 @@
+import heapq
+def solution(scoville, K):
+    summ = 0
+    scoville.sort()
+    while scoville:
+        a = heapq.heappop(scoville)
+        #아직 부족함
+        if a < K: 
+            if scoville:
+                b = heapq.heappop(scoville)
+                heapq.heappush(scoville, (a + b * 2))
+                summ += 1
+            else: 
+                return -1
+        else: 
+            return summ
+        
+    return -1
