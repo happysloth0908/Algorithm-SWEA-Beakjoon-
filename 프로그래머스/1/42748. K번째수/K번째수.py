8f2e790a5b3464451cb8515a67431dc2f55795ac
happysloth0908 <sozzykim@naver.com>
2026-02-04 14:16:10 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,14 +1,28 @@
+# def solution(array, commands):
+#     answer = []
+#     for command in commands:
+#         i = command[0]
+#         j = command[1]
+#         k = command[2]
+        
+#         cut = array[i -1: j ]
+#         print(cut)
+#         cut.sort()
+#         ans = cut[k - 1]
+#         answer.append(ans)
+        
+#     return answer
+
+1
+2
+3
+4
+5
+6
+7
 def solution(array, commands):
     answer = []
     for command in commands:
-        i = command[0]
-        j = command[1]
-        k = command[2]
-        
-        cut = array[i -1: j ]
-        print(cut)
-        cut.sort()
-        ans = cut[k - 1]
-        answer.append(ans)
-        
+        i,j,k = command
+        answer.append(sorted(array[i-1:j])[k-1])
     return answer
