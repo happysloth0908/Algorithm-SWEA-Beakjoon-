6ca2e75fcad3635c35802b4d78a79d79c91defd6
happysloth0908 <sozzykim@naver.com>
2026-02-05 15:55:07 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,32 +1,29 @@
--- 코드를 작성해주세요
-SELECT 
-    CASE 
-    WHEN EXISTS (
-        SELECT 1
-        FROM SKILLCODES s
-        WHERE s.CATEGORY = 'Front End' AND (s.CODE & d.SKILL_CODE) <> 0 
-    ) 
- 
-    AND EXISTS (
-        SELECT 1
-        FROM SKILLCODES ss
-        WHERE ss.NAME = 'Python' AND (d.SKILL_CODE & ss.CODE) <> 0 
-        ) THEN 'A'
- 
- 
- WHEN EXISTS 
-        (SELECT 1
-        FROM SKILLCODES sss
-        WHERE sss.NAME = 'C#' AND (d.SKILL_CODE & sss.CODE) <> 0 
-        ) THEN 'B'
-    WHEN EXISTS ( -- 그 외 Front End
-           SELECT 1
-           FROM SKILLCODES sfe2
-           WHERE sfe2.CATEGORY = 'Front End'
-             AND (d.SKILL_CODE & sfe2.CODE) <> 0 )
-      THEN 'C'
- END AS GRADE 
-, ID, EMAIL 
-FROM DEVELOPERS d
-HAVING GRADE is not null
-ORDER BY GRADE, d.ID
+# -- 코드를 작성해주세요
+# SELECT
+#   CASE 
+#     WHEN (SKILL_CODE & 256 > 0) and ( SKILL_CODE & 16 > 0 or SKILL_CODE & 2048 > 0 or SKILL_CODE & 8192 > 0 ) then 'A'
+#     WHEN SKILL_CODE & 1024 > 0 then 'B'
+#     WHEN SKILL_CODE & 16 > 0 or SKILL_CODE & 2048 > 0 or SKILL_CODE & 8192 > 0 then 'C'
+#     END AS GRADE
+# , ID, EMAIL
+# FROM DEVELOPERS
+# WHERE (SKILL_CODE & 256 > 0 and (SKILL_CODE & 16 > 0 or SKILL_CODE & 2048 > 0 or SKILL_CODE & 8192 > 0 )) or SKILL_CODE & 1024 > 0 
+# or (SKILL_CODE & 16 > 0 or SKILL_CODE & 2048 > 0 or SKILL_CODE & 8192 > 0)
+# ORDER BY 1, 2
+
+SELECT GRADE, ID, EMAIL
+FROM (
+    SELECT
+        CASE 
+            WHEN (D.SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = 'Python') > 0)
+                 AND (D.SKILL_CODE & (SELECT SUM(CODE) FROM SKILLCODES WHERE CATEGORY = 'Front End') > 0)
+                 THEN 'A'
+            WHEN D.SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = 'C#') > 0 THEN 'B'
+            WHEN D.SKILL_CODE & (SELECT SUM(CODE) FROM SKILLCODES WHERE CATEGORY = 'Front End') > 0 THEN 'C'
+        END AS GRADE,
+        D.ID, 
+        D.EMAIL
+    FROM DEVELOPERS D
+) AS SUB
+WHERE GRADE IS NOT NULL
+ORDER BY GRADE, ID;
