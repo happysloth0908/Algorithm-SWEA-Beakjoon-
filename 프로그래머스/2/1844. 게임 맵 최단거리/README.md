5a19f8a3bcafc3e97486e8d1c2ac60ae64088d38
happysloth0908 <sozzykim@naver.com>
2026-02-18 13:40:16 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 13.94 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844#) 
 
 ### 성능 요약
 
-메모리: 9.37 MB, 시간: 13.78 ms
+메모리: 9.36 MB, 시간: 13.94 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 18일 13:26:40
+2026년 02월 18일 13:40:12
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -3,31 +3,26 @@ def solution(maps):
     n = len(maps)
     m = len(maps[0])
     
-    answer = 0
     dr = [-1, 1, 0, 0]
     dc = [0,0,-1,1]
+    
     visited = [[0]*m for _ in range(n)]
     visited[0][0] = 1
+    
     que = deque()
     que.append((0,0))
     
     while que:
-        curr = que.popleft()
+        r,c = que.popleft()
+        if r == n-1 and c == m-1:
+            return visited[n-1][m-1]
         for i in range(4):
-            nr = curr[0] + dr[i]
-            nc = curr[1] + dc[i]
+            nr = r + dr[i]
+            nc = c + dc[i]
             
             if nr >= n or nc >= m or nr < 0 or nc < 0 or maps[nr][nc] == 0 or visited[nr][nc] != 0:
-                # print(nr, nc, "는 안 됨.")
                 continue
             
             que.append((nr,nc))
-            # print(nr, nc, "는 됨.")
-            visited[nr][nc] = visited[curr[0]][curr[1]] + 1
-            if nr == n-1 and nc == m-1:
-                # print("와 드디어 끝")
-                return visited[n-1][m-1]
-            
-            
-    print(visited)
+            visited[nr][nc] = visited[r][c] + 1
     return -1
