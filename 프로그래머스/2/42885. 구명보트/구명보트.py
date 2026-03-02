45f7fc3491944ae323608b9981e440ee9fdc20a6
happysloth0908 <sozzykim@naver.com>
2026-03-02 12:12:20 +0900

    [level 2] Title: 구명보트, Time: 10.36 ms, Memory: 9.57 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -4,25 +4,14 @@ def solution(people, limit):
     i = len(people) -1 
     j = 0
     boat = 0
-    escaped = set()
     while i >= 0 and i >= j:
-        # print(i,j)
-        if i == 0 and i not in escaped:
+        if i == 0:
             boat += 1
-            # print("0번 손님 태우고 이제 빠집니다")
             break
         
-        if i not in escaped: 
-            if limit - people[i] >= people[j] and j not in escaped:
-                escaped.add(j)
-                escaped.add(i)
-                j += 1
-                # print(i, j , "태운 배 하나 감")
-            else:
-                escaped.add(i)
-                # print(limit - people[i] >= people[j])
-                # print(i,"태운 배 하나 감")
-            boat += 1
+        if limit - people[i] >= people[j] :
+            j += 1
+        boat += 1
         
         i -= 1
         
