023ad4a4683936e2d0a6a182a113051bd10bc3b5
happysloth0908 <sozzykim@naver.com>
2026-03-03 15:19:17 +0900

    [level 2] Title: H-Index, Time: 0.14 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,18 +1,16 @@
-# def solution(citations):
-#     answer = 0
-#     citations.sort()
-#     if len(citations) == 1:
-#         return 1
-#     for i in range(len(citations)):
-#         if len(citations) - i <= citations[i]:
-#             # print("멈추기! 이 중 큰 건", citations[i-1], len(citations) - i, max(citations[i-1], len(citations) - i))
-#             return len(citations) - i
-
 def solution(citations):
+    answer = 0
     citations.sort()
-    n = len(citations)
-    for i, c in enumerate(citations):
-        h = n - i
-        if c >= h:
-            return h
+    for i in range(len(citations)):
+        if len(citations) - i <= citations[i]:
+            return len(citations) - i
     return 0
+
+# def solution(citations):
+#     citations.sort()
+#     n = len(citations)
+#     for i, c in enumerate(citations):
+#         h = n - i
+#         if c >= h:
+#             return h
+#     return 0
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
 
 ### 성능 요약
 
-메모리: 9.14 MB, 시간: 0.12 ms
+메모리: 9.13 MB, 시간: 0.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 03일 15:12:55
+2026년 03월 03일 15:19:14
 
 ### 문제 설명
 
